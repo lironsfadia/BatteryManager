64e036007bda5136c58ebb9e4fa2266b5bd90eba
Liron <liron.ssss@gmail.com>
2024-09-18 12:08:57 +0300

    first commit

---
+++ composeApp/src/androidMain/kotlin/org/example/expectactual/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.example.expectactual
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            App(batteryManager = remember {
+                BatteryManager(applicationContext)
+            })
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/org/example/expectactual/BatteryManager.kt
@@ -0,0 +1,5 @@
+package org.example.expectactual
+
+expect class BatteryManager {
+    fun getBatteryLevel():Int
+}
